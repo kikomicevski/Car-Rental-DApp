943ea03f9ef1b7d00eaf878dd6e97c48648e8dbf
kikomicevski <kiko.m.1996@live.com>
2023-09-05 11:28:03 +0200

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,43 +59,6 @@ function Navbar() {
               <img src={Logo} alt="logo-img" />
             </Link>
           </div>
-          {/* <ul className="navbar__links">
-            <li>
-              <Link className="home-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link className="about-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link className="models-link" to="/models">
-                Vehicle Models
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link className="testi-link" to="/testimonials">
-                Testimonials
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link className="team-link" to="/team">
-                Our Team
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link className="contact-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul> */}
           <div className="navbar__buttons">
             <Link className="navbar__buttons__sign-in" to="/">
               Sign In
